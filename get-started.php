83012e5def7047439f409fbe4e23d19fb41a052d
Bjorn Cooley <bjorncooley@gmail.com>
2015-05-13 15:31:08 -0700

    Add functions for rotating plan cards on Get Started page

--- get-started.php
+++ get-started.php
@@ -32,55 +32,57 @@
                 <img src="img/get-started/right-arrow-black.png" alt="" class="right-arrow">
                 <div class="row">
                     <h3 class="text-center padding-bottom-50">A) Choose a Base Plan</h3>
-                    <div class="small-12 medium-4 columns small-centered medium-uncentered plan-card">
-                        <div class="shadowed">
-                            <div class="card-text">
-                                <p class="padding-bottom-30">Under 3 Extensions</p>
-                                <h4>$17 / Extension / Month</h4>
-                                <p>(Includes 30 Days Free)</p>
-                            </div>
-                            <div class="text-center">
-                                <img src="img/get-started/yellow-plus-icon-mobile.png" alt="">
-                            </div>
-                        </div>
-                        
-                        <div class="red-orange-background white text-center padding-top-30 padding-bottom-30">
-                            <h4 class="uppercase">Startup</h4>
-                        </div>
-                    </div>
-                    <div class="small-12 medium-4 columns small-centered plan-card medium-uncentered">
-                        <div class="shadowed">
-                            <div class="card-text">
-                                <p class="padding-bottom-30">3-40 Extensions</p>
-                                <h4>$35 / Month</h4>
-                                <div class="inline-block">
-                                    <h4>$25 / Month</h4>
-                                    <p>(1 yr. Prepaid)</p>
+                    <div class="card-container relative">
+                        <div class="small-12 medium-4 columns small-centered medium-uncentered plan-card current">
+                            <div class="shadowed">
+                                <div class="card-text">
+                                    <p class="padding-bottom-30">Under 3 Extensions</p>
+                                    <h4>$17 / Extension / Month</h4>
+                                    <p>(Includes 30 Days Free)</p>
+                                </div>
+                                <div class="text-center">
+                                    <img src="img/get-started/yellow-plus-icon-mobile.png" alt="">
                                 </div>
                             </div>
-                            <div class="text-center">
-                                <img src="img/get-started/yellow-plus-icon-mobile.png" alt="">
+                            
+                            <div class="red-orange-background white text-center padding-top-30 padding-bottom-30">
+                                <h4 class="uppercase">Startup</h4>
                             </div>
                         </div>
-                        
-                        <div class="red-orange-background white text-center padding-top-30 padding-bottom-30">
-                            <h4 class="uppercase">Small Business</h4>
-                        </div>
-                    </div>
-                    <div class="small-12 medium-4 columns small-centered plan-card medium-uncentered">
-                        <div class="shadowed">
-                            <div class="card-text">
-                                <p class="padding-bottom-30">41+ Extensions</p>
-                                <h4>Call Us!</h4>
-                                <p><a href="tel:5052533451">(505) 253-3451</a></p>
+                        <div class="small-12 medium-4 columns small-centered plan-card medium-uncentered">
+                            <div class="shadowed">
+                                <div class="card-text">
+                                    <p class="padding-bottom-30">3-40 Extensions</p>
+                                    <h4>$35 / Month</h4>
+                                    <div class="inline-block">
+                                        <h4>$25 / Month</h4>
+                                        <p>(1 yr. Prepaid)</p>
+                                    </div>
+                                </div>
+                                <div class="text-center">
+                                    <img src="img/get-started/yellow-plus-icon-mobile.png" alt="">
+                                </div>
                             </div>
-                            <div class="text-center">
-                                <img src="img/get-started/yellow-plus-icon-mobile.png" alt="">
+                            
+                            <div class="red-orange-background white text-center padding-top-30 padding-bottom-30">
+                                <h4 class="uppercase">Small Business</h4>
                             </div>
                         </div>
-                        
-                        <div class="red-orange-background white text-center padding-top-30 padding-bottom-30">
-                            <h4 class="uppercase">Enterprise</h4>
+                        <div class="small-12 medium-4 columns small-centered plan-card medium-uncentered">
+                            <div class="shadowed">
+                                <div class="card-text">
+                                    <p class="padding-bottom-30">41+ Extensions</p>
+                                    <h4>Call Us!</h4>
+                                    <p><a href="tel:5052533451">(505) 253-3451</a></p>
+                                </div>
+                                <div class="text-center">
+                                    <img src="img/get-started/yellow-plus-icon-mobile.png" alt="">
+                                </div>
+                            </div>
+                            
+                            <div class="red-orange-background white text-center padding-top-30 padding-bottom-30">
+                                <h4 class="uppercase">Enterprise</h4>
+                            </div>
                         </div>
                     </div>
                 </div>
--- js/functions.js
+++ js/functions.js
@@ -1,5 +1,100 @@
+var card_margin=0;
+var cards_moved = false;
+
+$(window).load(function(){
+    var plan_card_height = $('.plan-card').height();
+    var container_height = $('.card-container').height();
+
+    $('.card-container').height(container_height - plan_card_height * 2);
+
+    // Check whether we need to rearrange the plan cards on the
+    // get started page
+    movePlanCards();
+
+    
+});
+
+$(window).resize(function(){
+
+    movePlanCards();
+});
+
 $(function(){
+
+    // Down arrow for Home Page
+
     $('#hero-down-arrow').click(function(){
         $('html, body').animate({ scrollTop: $('#intro').offset()['top']}, 800);
     });
+
+    // Card swapping for Get Started page
+
+    $('#step-2 .left-arrow').click(function(){
+
+        var $current = $('.plan-card.current');
+        var $next = $current.next();
+        var width = $(window).width();
+        var offset = width + card_margin;
+
+        // Check if this is the first card, if so, get the last card
+        if ( $next.length == 0 ) {
+
+            $next = $('.plan-card:first');
+        }
+
+        // Make sure the next card is in the correct position
+        $next.css('left', offset + 'px');
+
+        $current.animate({ left: "-=" + width,}, 800).removeClass('current');
+        $next.animate({ left: "-=" + width,}, 800).addClass('current');
+    });
+
+    $('#step-2 .right-arrow').click(function(){
+
+        var $current = $('.plan-card.current');
+        var $next = $current.prev();
+        var width = $(window).width();
+        var offset = width + card_margin;
+
+        // Check if this is the first card, if so, get the last card
+        if ( $next.length == 0 ) {
+
+            $next = $('.plan-card:last');
+        }
+
+        // Make sure the next card is in the correct position
+        $next.css('left', '-' + width + 'px');
+
+        $current.animate({ left: "+=" + offset,}, 800).removeClass('current');
+        $next.animate({ left: "+=" + offset,}, 800).addClass('current');
+    });
 });
+
+function movePlanCards() {
+
+    if ( $(window).width() < 640 ) {
+
+        cards_moved = true;
+
+        var width = $(window).width();
+        var padding = parseInt( $('.plan-card.current').css('padding-left') );
+        card_margin = (width - $('.plan-card.current').width()) / 2 - padding;
+        var i=0;
+
+        $('.plan-card').each(function(){
+
+            offset = (width * i) + card_margin;
+            $(this).css('position', 'absolute');
+            $(this).css('left', offset + 'px');
+
+            i += 1;
+        });
+    } else if ( cards_moved = true ) {
+
+        $('.plan-card').each(function(){
+            $(this).css('position', 'static');
+        });
+
+        cards_moved = false;
+    }
+}
