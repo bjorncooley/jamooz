79da88e0b3b3be7840d6cb5282dc04fcf64246da
Bjorn Cooley <bjorncooley@gmail.com>
2015-05-13 23:50:34 -0700

    Add desktop styling for Hardware Integration page

--- hardware-integrations.php
+++ hardware-integrations.php
@@ -26,8 +26,9 @@
 </div>
 <div id="story-1">
     <div class="row">
-        <div class="small-12 large-6 columns">
+        <div class="small-12 large-6 columns relative">
             <img src="img/hardware/mary_mobile.png" alt="">
+            <img src="img/hardware/phone-icon.png" alt="" id="story-1-phone-icon" class="show-for-large-up">
         </div>
         <div class="small-12 large-6 columns">
             <h3>Mary is having a great day at work. She is on a call on the landline,
@@ -37,37 +38,44 @@
 </div>
 <div id="story-2">
     <div class="row">
-        <div class="small-12 large-6 columns">
+        <div class="small-12 large-6 large-offset-1 columns">
             <img src="img/hardware/alex_mobile.png" alt="" class="hide-for-large-up">
-            <img src="img/hardware/alex_mobile_2.png" alt="">
+            <img src="img/hardware/alex_mobile_2.png" alt="" class="hide-for-large-up">
             <img src="img/hardware/alex_desktop.png" alt="" class="show-for-large-up">            
         </div>
-        <div class="small-12 large-6 columns">
+        <div class="small-12 large-5 columns">
             <h3>Alex sees on his cell phone that there's a call from David, a hot lead.
             He's on his way to the office. I am running a bit late.</h3>
         </div>
     </div>
     <div class="row">
-        <div class="small-12 columns">
-            <img src="img/hardware/david_mobile.png" alt="" class="hide-for-large-up">
-            <img src="img/hardware/david_desktop.png" alt="" class="show-for-large-up">
+        <div class="small-12 columns hide-for-large-up">
+            <img src="img/hardware/david_mobile.png" alt="">
+        </div>
+        <div class="large-11 columns large-offset-1 inline-block show-for-large-up padding-top-30">
+            <img src="img/hardware/david_desktop_1.png" alt="">
+            <img src="img/hardware/david_desktop_2.png" alt="">
+            <img src="img/hardware/david_desktop_3.png" alt="">
+            <img src="img/hardware/walking_icon_2.png" alt="" id="walking-icon-2">
         </div>
     </div>
 </div>
 <div id="story-3">
-    <div class="small-12 large-6 columns">
-        <h3>David stops by the office later that day. He rings the intercom,
-        and Mary sees him on her computer. Come on in David!</h3>
-    </div>
-    <div class="small-12 large-6 columns">
-        <img src="img/hardware/david_intercom_mobile.png" alt="" class="hide-for-large-up">
-        <img src="img/hardware/david_intercom_desktop.png" alt="" class="show-for-large-up">
+    <div class="row">
+        <div class="small-12 large-5 columns">
+            <h3>David stops by the office later that day. He rings the intercom,
+            and Mary sees him on her computer. Come on in David!</h3>
+        </div>
+        <div class="small-12 large-6 large-offset-1 columns">
+            <img src="img/hardware/david_intercom_mobile.png" alt="" class="hide-for-large-up">
+            <img src="img/hardware/david_intercom_desktop.png" alt="" class="show-for-large-up">
+        </div>
     </div>
 </div>
 <div id="all-in-one">
     <div class="row">
         <div class="small-12 large-6 columns">
-            <h2>The all-in-one Communications Solution</h2>
+            <h2 class="large">The all-in-one Communications Solution</h2>
             <p class="padding-top-30 padding-bottom-15">
                 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quis illum, dolore, ipsam unde sint qui illo quam numquam, quos aspernatur nostrum ratione assumenda cum! Voluptatem corporis sequi, neque illum veritatis.
             </p>
@@ -148,28 +156,30 @@
 </div><!-- #discounts -->
 <div id="hardware-support">
     <div class="row">
-        <div class="small-12 columns text-center padding-bottom-30">
+        <div class="large-12 columns show-for-large-up border-top"></div>
+        <div class="small-12 columns text-center">
             <h3>Got Questions? See our <a href="#" target="_blank">Guide</a> here or contact
             <a href="#" target="_blank">Support</a> directly.</h3>
         </div>
-        <div class="row">
-            <div class="small-12 large-10 columns large-centered">
-                <div class="row">
-                    <div class="small-12 large-6 columns white teal-background padding-top-80 padding-bottom-80 text-center">
-                        <h4 class="white">
-                            <div class="small-9 columns small-centered padding-bottom-30">First time guest?</div>
-                        </h4>
-                        <a href="#" class="white-border white uppercase text-center small">Get Started</a>
-                    </div>
-                    <div class="small-12 large-6 columns white medium-dark-gray-background padding-top-80 padding-bottom-80 text-center">
-                        <h4 class="white text-center">
-                            <div class="small-9 columns small-centered padding-bottom-30">Returning Customer</div>
-                        </h4>
-                        <a href="#" class="white-border white uppercase small">Login</a>
-                    </div>
+    </div>
+    <div class="row">
+        <div class="small-12 large-10 columns large-centered">
+            <div class="row">
+                <div class="small-12 large-6 columns white teal-background padding-top-80 padding-bottom-80 text-center">
+                    <h4 class="white">
+                        <div class="small-9 columns small-centered padding-bottom-30">First time guest?</div>
+                    </h4>
+                    <a href="#" class="white-border white uppercase text-center small">Get Started</a>
+                </div>
+                <div class="small-12 large-6 columns white medium-dark-gray-background padding-top-80 padding-bottom-80 text-center">
+                    <h4 class="white text-center">
+                        <div class="small-9 columns small-centered padding-bottom-30">Returning Customer</div>
+                    </h4>
+                    <a href="#" class="white-border white uppercase small">Login</a>
                 </div>
             </div>
         </div>
+    </div>
         
     </div>
 </div>
