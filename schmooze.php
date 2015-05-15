5568197a5b228f95d66d96dbab7afa2483dfb66a
Bjorn Cooley <bjorncooley@gmail.com>
2015-05-15 00:43:15 -0700

    Add Schmooze page

---
+++ schmooze.php
@@ -0,0 +1,132 @@
+<!DOCTYPE html>
+<html class="no-js" lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>Jamooz</title>
+        <link rel="stylesheet" href="css/foundation/foundation.css" />
+        <link rel="stylesheet" href="css/custom/styles.css">
+        
+    </head>
+    <body>
+
+        <div id="schmooze-page">
+
+            <header></header>
+            <div id="nav">
+                <div class="row padding-bottom-30 padding-top-30">
+                    <div class="large-3 columns">
+                        <img src="img/shared/jamooz_small_logo.png" alt="">
+                    </div>
+                    <div class="large-9 columns clearfix text-right padding-top-15">
+                            <button class="no-background dark-gray-border dark-gray radius large-3 columns small-centered uppercase">Login</button>
+                            <button class="radius large-3 columns small-centered mint no-background mint-border uppercase">Get Early Access</button>
+                        
+                    </div>
+                </div>
+            </div>
+
+            <div id="schmooze-intro" class="row">
+                <div class="large-8 columns">
+                    <h1 class="mint padding-top-50 padding-bottom-50 no-text-transform large">Schmooze by Jamooz</h1>
+                    <h3 class="small">Jamooz technology, curated. Phone, messaging, voicemail,
+                    &amp; file sharing redesigned for personal use &amp; team collaboration.</h3>
+                    <h3 class="medium-blue padding-top-50 padding-bottom-50">Coming soon.</h3>
+                    <form action="" method="post" class="inline-block">
+                        <input type="text" placeholder="Email address">
+                        <button class="bright-green-background white uppercase">Get Early Access</button>
+                    </form>
+                </div>
+                <div class="large-4 columns">
+                    <img src="img/schmooze/phone.png" alt="">
+                    <p class="tiny text-center">Native Apps for Android &amp; iOS</p>
+                </div>
+                <div class="row">
+                    <div class="large-12 columns text-center padding-top-80">
+                        <p class="small uppercase">Learn More</p>
+                        <img src="img/shared/down_arrow_black.png" alt="">
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="large-12 columns text-center padding-top-80">
+                    <h2>Do better work with organized team communication.</h2>
+                </div>
+                <div class="large-8 columns text-center">
+                    <img src="img/schmooze/monitor.png" alt="">
+                    <p class="tiny text-center">Windows &amp; Mac Native Desktop Apps</p>
+                </div>
+                <div class="large-4 columns">
+                    <p>Seamlessly flip between your private communication and the public pulse.</p> 
+                    <p>Creating a flow of communication across your team or call center is now a lot simpler. Setup preferences for automation of calls, messages, file sharing or voicemails coming from specific channels that you want in the public pulse. Or simply unlock something in your personal communications to make it accessible.</p>
+                    <button class="mint-border mint no-background width-285">Get early access</button>
+                </div>
+            </div>
+            <div id="communications">
+                <div class="row">
+                    <div class="large-12 columns text-center">
+                        <h2 class="padding-bottom-50">Stay on top of all your communications with less headache and time</h2>
+                        <img src="img/schmooze/screens_graphic.png" alt="">
+                    </div>
+                </div>
+            </div>
+            
+            <div class="row">
+                <div class="large-12 columns text-center">
+                    <h3 class="small mint padding-top-50 padding-bottom-15">Software Integrations that make your communication faster</h3>
+                    <img src="img/schmooze/integrations.png" alt="" class="padding-bottom-50">
+                    <p>Jamooz takes analytics to the next step with actions. See where all of your team's efforts are being put. Then share the stats with your team to
+                    highlight areas of improvement.</p>
+                </div>
+            </div>
+            <div class="row">
+                <div class="large-12 columns text-center">
+                    <h3 class="small mint padding-bottom-50">Simple to set up with typical office hardware</h3>
+                    <img src="img/schmooze/hardware.png" alt="" class="padding-bottom-30">
+                    <p>Jamooz takes analytics to the next step with actions. See where all of your team's efforts are being put. Then share the stats with your team to
+                    highlight areas of improvement.</p>
+                </div>
+            </div>
+            <div class="row">
+                <div class="large-12 columns text-center">
+                    <img src="img/schmooze/graphs.png" alt="" class="padding-top-80 padding-bottom-50">
+                    <h3 class="small mint padding-bottom-30">Smart analytics, intuitive design.</h3>
+                    <p>Jamooz takes analytics to the next step with actions. See where all of your team's efforts are being put. Then share the stats with your team to
+                    highlight areas of improvement.</p>
+                </div>
+            </div>  
+            <div class="row">
+                <div class="large-12 columns text-center">
+                    <img src="img/schmooze/timeline.png" alt="" class="padding-top-80 padding-bottom-50">
+                    <h3 class="small mint padding-bottom-30">Archive conversations, easy searchability</h3>
+                    <p class="padding-bottom-80">Jamooz takes analytics to the next step with actions. See where all of your team's efforts are being put. Then share the stats with your team to
+                    highlight areas of improvement.</p>
+                </div>
+            </div>  
+            <div id="about-jamooz" class="medium-dark-gray-background">
+                <div class="row">
+                    <div class="large-12 columns text-center white">
+                        <h3 class="small padding-bottom-80">Jamooz is a telecommunications company reinventing the way
+                        small businesses and enterprises interact and grow. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio eaque, accusamus pariatur.</h3>
+                        <h4 class="white-border white padding-top-8 padding-bottom-8 max-285 text-center">Learn more about Jamooz</h4>
+                    </div>
+                </div>
+            </div>
+            <div id="coming-soon" class="bright-green-background white text-center">
+                <h3>Coming soon in 2015.</h3>
+                <h5 class="padding-bottom-50">Get early access today. Fill in to reserve a limited spot.</h5>
+                <input type="text" placeholder="Email address">
+                <div class="padding-top-50">
+                    <button class="red-orange-background white uppercase">
+                        Get Early Access Today
+                    </button>
+                </div>
+                
+                <p class="tiny white">a customer success teammate will be in touch</p>
+            </div>
+            <div id="support" class="dark-blue-background text-center inline-block">
+                <h4 class="uppercase white-border white">Need Support? Contact Us Here</h4>
+            </div>
+        </div>
+
+<?php include('includes/footer.php'); ?>
