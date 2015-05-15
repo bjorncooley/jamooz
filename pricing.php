a36434187f4d5ee231c764851de2d7842e9716ef
Bjorn Cooley <bjorncooley@gmail.com>
2015-05-14 17:30:21 -0700

    Add inital pricing page

---
+++ pricing.php
@@ -0,0 +1,20 @@
+<?php include('includes/header.php'); ?>
+
+<div id="pricing">
+    <div id="pricing-hero" class="padding-top-30 padding-bottom-15">
+        <div class="row white">
+            <div class="large-12 columns">
+                <h1 class="large">Transparent Pricing<br />Customizable Plans</h1>
+            </div>
+            <div class="large-12 columns padding-top-30">
+                <h4 class="light">Pricing should be both flexible to needs, and fair for 
+                customers of all shapes and sizes. Transparency is what makes this possible.</h4>
+            </div>
+            <div class="large-12 columns text-center padding-top-30">
+                <img src="img/shared/white_down_arrow.png" alt="">
+            </div>
+        </div>
+    </div>
+</div>
+
+<?php include('includes/footer.php'); ?>
