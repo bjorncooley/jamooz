66139779868baa7bc9f4e9fcb4baa0e8dd2f4846
Bjorn Cooley <bjorncooley@gmail.com>
2015-05-15 01:58:32 -0700

    Add Success landing page

---
+++ success.php
@@ -0,0 +1,97 @@
+<!DOCTYPE html>
+<html class="no-js" lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>Jamooz</title>
+        <link rel="stylesheet" href="css/foundation/foundation.css" />
+        <link rel="stylesheet" href="css/custom/styles.css">
+        
+    </head>
+    <body class="relative">
+        <header></header>
+
+        <div id="success-page" class="inline-block">
+            <div class="light-gray-background" id="sidebar">
+                <div class="inline-block side-menu-header">
+                    <img src="img/success/jamooz_logo.png" alt="">
+                    <h4 class="large">Success Center</h4>
+                </div>
+                <div class="side-menu-item">
+                    <h4 class="uppercase">Setup &amp; Basics</h4>
+                </div>
+                <div class="side-menu-item">
+                    <h4 class="uppercase">Do More</h4>
+                </div>
+                <div class="side-menu-item">
+                    <h4 class="uppercase">Troubleshooting</h4>
+                </div>
+            </div>
+            <div id="main-content">
+                <div id="nav">
+                    <div class="row padding-bottom-30 padding-top-30">
+                        <div class="large-12 columns text-right">
+                            <button class="no-background dark-gray-border dark-gray radius uppercase">Login</button>
+                            <button class="radius red-orange no-background red-orange-border uppercase">Sign Up</button>
+                        </div>
+                    </div>
+                </div>
+                <div id="welcome">
+                    <div class="row">
+                        <div class="large-6 large-offset-1 columns text-left">
+                            <h2 class="uppercase padding-bottom-30 red-orange large strong">Welcome To The Success Center</h2>
+                            <p class="padding-bottom-30">From getting a solid foundation of the basics to
+                            mastering the advanced capabilities of Jamooz products,
+                            we're here to help</p>
+                            <button class="white red-orange-background uppercase">Get Started</button>
+                        </div>
+                    </div>
+                </div>
+                <div id="search-bar" class="teal-background">
+                    <div class="row">
+                        <div class="large-8 large-offset-2 columns">
+                            <input type="text" placeholder="I'd like support with..." class="white">
+                        </div>
+                    </div>
+                </div>
+                <div id="footer-options">
+                    <div class="row">
+                        <div class="large-4 columns text-center">
+                            <h4 class="uppercase">Master the Basics</h4>
+                        </div>
+                        <div class="large-4 columns text-center">
+                            <h4 class="uppercase">Get More Done</h4>
+                        </div>
+                        <div class="large-4 columns text-center">
+                            <h4 class="uppercase">Troubleshooting</h4>
+                        </div>
+                    </div>
+                </div>
+                <div id="success-footer">
+                    <div class="inline-block medium-blue-background white">
+                        <img src="img/success/white_left_arrow.png" alt="">
+                        <div class="block">
+                            <p class="small">previous</p>
+                            <p>Jamooz Home</p>
+                        </div>
+                    </div>
+                    <div class="text-right orange-background white">
+                        <div class="block">
+                            <p class="small">next</p>
+                            <p>Getting Started</p>
+                        </div>
+                        
+                        <img src="img/success/white_right_arrow.png" alt="">
+                    </div>
+                    
+                </div>
+            </div>
+        </div>
+        <footer>
+            <link href='http://fonts.googleapis.com/css?family=Roboto:500,400,300,100' rel='stylesheet' type='text/css'>
+            <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+            <script src="js/vendor/modernizr.js"></script>
+            <script src="js/functions.js"></script>
+        </footer>
+    </body>
+</html>
