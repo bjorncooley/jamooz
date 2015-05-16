43701293b879e4b6eba82a382258b59628609584
Bjorn Cooley <bjorncooley@gmail.com>
2015-05-16 13:58:24 -0700

    Move #extras section on Pricing page to apps-add-ons partial

---
+++ includes/apps-add-ons.php
@@ -0,0 +1,74 @@
+<div id="extras" class="relative slide-container">
+    <div class="dark-blue-background white current slide">
+        <img src="img/shared/slider_right_arrow_white.png" alt="" class="right-arrow">
+        <img src="img/shared/slider_left_arrow_white.png" alt="" class="left-arrow">
+        <div class="row">
+            <div class="small-12 large-6 columns">
+                <h1 class="no-text-transform light">Efax Pro.</h1>
+                <p class="light">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Excepturi libero sed, facere a ipsam. Eveniet iure, recusandae eligendi, ex magni aperiam ipsa provident quam dicta quae temporibus voluptate porro eos?</p>
+            </div>
+            <div class="small-12 large-6 columns show-for-large-up">
+                <img src="img/pricing/efax_icon.png" alt="">
+            </div>
+            <div class="small-12 columns text-center add-container">
+                <h5>$20/Month</h5>
+                <img src="img/shared/yellow_plus_icon.png" alt="">
+                <p class="small uppercase">Add to Plan</p>
+            </div>
+        </div>
+    </div>
+    <div class="light-gray-background slide">
+        <img src="img/shared/slider_right_arrow_black.png" alt="" class="right-arrow">
+        <img src="img/shared/slider_left_arrow_black.png" alt="" class="left-arrow">
+        <div class="row">
+            <div class="small-12 large-6 columns show-for-large-up">
+                <img src="img/pricing/efax_icon.png" alt="">
+            </div>
+            <div class="small-12 large-6 columns">
+                <h1 class="no-text-transform light">Call Center Queue &amp; Reporting (ACD)</h1>
+                <p class="light">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Excepturi libero sed, facere a ipsam. Eveniet iure, recusandae eligendi, ex magni aperiam ipsa provident quam dicta quae temporibus voluptate porro eos?</p>
+            </div>
+            <div class="small-12 columns text-center add-container">
+                <h5>$10/Month</h5>
+                <img src="img/shared/yellow_plus_icon.png" alt="">
+                <p class="small uppercase">Add to Plan</p>
+            </div>
+        </div>
+    </div>
+    <div class="dark-blue-background white slide">
+        <img src="img/shared/slider_right_arrow_white.png" alt="" class="right-arrow">
+        <img src="img/shared/slider_left_arrow_white.png" alt="" class="left-arrow">
+        <div class="row">
+            <div class="small-12 large-6 columns">
+                <h1 class="no-text-transform light">Call Recording.</h1>
+                <p class="light">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Excepturi libero sed, facere a ipsam. Eveniet iure, recusandae eligendi, ex magni aperiam ipsa provident quam dicta quae temporibus voluptate porro eos?</p>
+            </div>
+            <div class="small-12 large-6 columns show-for-large-up">
+                <img src="img/pricing/efax_icon.png" alt="">
+            </div>
+            <div class="small-12 columns text-center add-container">
+                <h5>$20/Month</h5>
+                <img src="img/shared/yellow_plus_icon.png" alt="">
+                <p class="small uppercase">Add to Plan</p>
+            </div>
+        </div>
+    </div>
+    <div class="light-gray-background slide">
+        <img src="img/shared/slider_right_arrow_black.png" alt="" class="right-arrow">
+        <img src="img/shared/slider_left_arrow_black.png" alt="" class="left-arrow">
+        <div class="row">
+            <div class="small-12 large-6 columns show-for-large-up">
+                <img src="img/pricing/efax_icon.png" alt="">
+            </div>
+            <div class="small-12 large-6 columns">
+                <h1 class="no-text-transform light">Administrative App</h1>
+                <p class="light">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Excepturi libero sed, facere a ipsam. Eveniet iure, recusandae eligendi, ex magni aperiam ipsa provident quam dicta quae temporibus voluptate porro eos?</p>
+            </div>
+            <div class="small-12 columns text-center add-container">
+                <h5>$10/Month</h5>
+                <img src="img/shared/yellow_plus_icon.png" alt="">
+                <p class="small uppercase">Add to Plan</p>
+            </div>
+        </div>
+    </div>
+</div>
