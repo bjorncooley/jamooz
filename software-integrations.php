dc825c48b9636f12e329ee33d40b58388f18ee5b
Bjorn Cooley <bjorncooley@gmail.com>
2015-05-27 21:49:23 -0700

    Refactor software integrations to use trial.php template'

--- software-integrations.php
+++ software-integrations.php
@@ -224,34 +224,7 @@
             
         </div>
     </div>
-    <div id="software-trial" class="light-gray-background padding-bottom-50 padding-top-15">
-        <div class="row">
-            <div class="small-12 columns text-center padding-top-50 padding-bottom-50">
-                <h4>We give every new customer a free 30 day trial to test run our service
-                with 1-3 extensions. Get started with a Customer Success Teammate.</h4>
-            </div>
-            <div class="small-12 columns text-center">
-                <form action="" method="post">
-                    <div class="row">
-                        <div class="small-12 large-6 columns">
-                            <input type="text" name="full_name" placeholder="Your Full Name">
-                        </div>
-                        <div class="small-12 large-6 columns">
-                            <input type="email" name="email" placeholder="Email address">
-                        </div>
-                        <div class="small-12 columns padding-top-30 padding-bottom-15">
-                            <button type="submit" class="red-orange-background white uppercase">
-                                Get Started For 30-Days Free
-                            </button>
-                        </div>
-                    </div>
-                </form>
-            </div>
-            <div class="small-12 columns text-center">
-                <p class="small">a customer success teammate is ready to help</p>
-            </div>
-        </div>
-    </div>
+    <?php include('./includes/trial.php'); ?>
 </div>
 
 
