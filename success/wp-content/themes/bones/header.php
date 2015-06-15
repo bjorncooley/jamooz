33bc3391364275dba5357f14d1a3077e1236e724
Bjorn Cooley <bjorncooley@gmail.com>
2015-06-15 16:35:27 -0700

    Add searchform, login/signup buttons to sucess template header

--- success/wp-content/themes/bones/header.php
+++ success/wp-content/themes/bones/header.php
@@ -49,10 +49,19 @@
 				<div id="inner-header" class="wrap cf">
 
 					<?php // to use a image just replace the bloginfo('name') with your img src and remove the surrounding <p> ?>
-					<p id="logo" class="h1" itemscope itemtype="http://schema.org/Organization"><a href="<?php echo home_url(); ?>" rel="nofollow"><?php bloginfo('name'); ?></a></p>
+					<!-- <p id="logo" class="h1" itemscope itemtype="http://schema.org/Organization"><a href="<?php //echo home_url(); ?>" rel="nofollow"><?php //bloginfo('name'); ?></a></p> -->
 
 					<?php // if you'd like to use the site description you can un-comment it below ?>
 					<?php // bloginfo('description'); ?>
+
+                    <?php include (TEMPLATEPATH.'/searchform.php'); ?>
+
+                    <?php $url = get_site_url(); ?>
+                    <?php $link = $url . '/../index.php#trial' ?>
+                    <button class="radius large-3 columns medium-dark-gray-border no-background medium-dark-gray small-centered uppercase login-button">Login</button>
+                    <a href="<?php echo $link ?>">
+                        <button class="radius large-3 columns small-centered orange no-background orange-border uppercase">Sign Up</button>
+                    </a>
 
 
 					<nav role="navigation" itemscope itemtype="http://schema.org/SiteNavigationElement">
