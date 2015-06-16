456518db815f03e6ca708d117da02717cfa36745
Bjorn Cooley <bjorncooley@gmail.com>
2015-06-15 23:42:24 -0700

    Update header styles for Success template

--- success/wp-content/themes/bones/header.php
+++ success/wp-content/themes/bones/header.php
@@ -46,40 +46,42 @@
 
 			<header class="header" role="banner" itemscope itemtype="http://schema.org/WPHeader">
 
-				<div id="inner-header" class="wrap cf">
+				<div id="inner-header" class="wrap cf group">
 
-					<?php // to use a image just replace the bloginfo('name') with your img src and remove the surrounding <p> ?>
-					<!-- <p id="logo" class="h1" itemscope itemtype="http://schema.org/Organization"><a href="<?php //echo home_url(); ?>" rel="nofollow"><?php //bloginfo('name'); ?></a></p> -->
+                    <div class="brand">
+                        <img src="../images/logo.png" alt="">
+                    </div>
 
-					<?php // if you'd like to use the site description you can un-comment it below ?>
-					<?php // bloginfo('description'); ?>
+                    <div class="main-header">
+                        <?php include (TEMPLATEPATH . '/searchform.php'); ?>
 
-                    <?php include (TEMPLATEPATH . '/searchform.php'); ?>
-
-                    <?php $url = get_site_url(); ?>
-                    <?php $link = $url . '/../index.php#trial' ?>
-                    <button class="radius large-3 columns medium-dark-gray-border no-background medium-dark-gray small-centered uppercase login-button">Login</button>
-                    <a href="<?php echo $link ?>">
-                        <button class="radius large-3 columns small-centered orange no-background orange-border uppercase">Sign Up</button>
-                    </a>
+                        <?php $url = get_site_url(); ?>
+                        <?php $link = $url . '/../index.php#trial' ?>
+                        <button class="radius large-3 columns medium-dark-gray-border no-background medium-dark-gray small-centered uppercase login-button">Login</button>
+                        <a href="<?php echo $link ?>">
+                            <button class="radius large-3 columns small-centered orange no-background red-orange-border uppercase">Sign Up</button>
+                        </a>
 
 
-					<nav role="navigation" itemscope itemtype="http://schema.org/SiteNavigationElement">
-						<?php wp_nav_menu(array(
-    					         'container' => false,                           // remove nav container
-    					         'container_class' => 'menu cf',                 // class of container (should you choose to use it)
-    					         'menu' => __( 'The Main Menu', 'bonestheme' ),  // nav name
-    					         'menu_class' => 'nav top-nav cf',               // adding custom nav class
-    					         'theme_location' => 'main-nav',                 // where it's located in the theme
-    					         'before' => '',                                 // before the menu
-        			               'after' => '',                                  // after the menu
-        			               'link_before' => '',                            // before each link
-        			               'link_after' => '',                             // after each link
-        			               'depth' => 0,                                   // limit the depth of the nav
-    					         'fallback_cb' => ''                             // fallback function (if there is one)
-						)); ?>
+                        <nav role="navigation" itemscope itemtype="http://schema.org/SiteNavigationElement">
+                            <?php wp_nav_menu(array(
+                                     'container' => false,                           // remove nav container
+                                     'container_class' => 'menu cf',                 // class of container (should you choose to use it)
+                                     'menu' => __( 'The Main Menu', 'bonestheme' ),  // nav name
+                                     'menu_class' => 'nav top-nav cf',               // adding custom nav class
+                                     'theme_location' => 'main-nav',                 // where it's located in the theme
+                                     'before' => '',                                 // before the menu
+                                       'after' => '',                                  // after the menu
+                                       'link_before' => '',                            // before each link
+                                       'link_after' => '',                             // after each link
+                                       'depth' => 0,                                   // limit the depth of the nav
+                                     'fallback_cb' => ''                             // fallback function (if there is one)
+                            )); ?>
 
-					</nav>
+                        </nav>
+                    </div>
+
+                    
 
 				</div>
 
