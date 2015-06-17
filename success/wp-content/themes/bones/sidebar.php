7fad489c47f7280627e40b45f37ab9abca9fca0d
Bjorn Cooley <bjorncooley@gmail.com>
2015-06-17 13:40:26 -0700

    Update Sucess sidebar to iterate through all categories, display posts for each category with permalink

--- success/wp-content/themes/bones/sidebar.php
+++ success/wp-content/themes/bones/sidebar.php
@@ -1,21 +1,26 @@
 				<div id="sidebar1" class="sidebar m-all t-1of3 d-2of7 last-col cf" role="complementary">
 
-					<?php if ( is_active_sidebar( 'sidebar1' ) ) : ?>
+					<?php $categories = get_categories(); ?>
+					<?php foreach( $categories as $category ) {
 
-						<?php dynamic_sidebar( 'sidebar1' ); ?>
+						$post_args = array(
 
-					<?php else : ?>
+							'category' => $category->cat_ID,
+						);
 
-						<?php
-							/*
-							 * This content shows up if there are no widgets defined in the backend.
-							*/
-						?>
+						$posts_array = get_posts($post_args);
 
-						<div class="no-widgets">
-							<p><?php _e( 'This is a widget ready area. Add some and they will appear here.', 'bonestheme' );  ?></p>
-						</div>
+						echo '<div class="category">';
+						
+						echo '<h2>' . $category->name . '</h2>';
 
-					<?php endif; ?>
+						foreach ( $posts_array as $post ) : setup_postdata( $post ); ?>
+							<h5>
+								<a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+							</h5>
+						<?php endforeach; 
+
+						echo '</div>';
+					} ?>
 
 				</div>
