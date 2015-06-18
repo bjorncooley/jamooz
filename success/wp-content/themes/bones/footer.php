12a27b1d43c22f226d1798935ccc3ed6838ee4de
Bjorn Cooley <bjorncooley@gmail.com>
2015-06-17 19:29:29 -0700

    Add links to Success footer

--- success/wp-content/themes/bones/footer.php
+++ success/wp-content/themes/bones/footer.php
@@ -5,25 +5,42 @@
                     <p>&copy; Copyright <?php echo date('Y'); ?></p>            
                 </div>
                 <div id="footer-right" class="clearfix">
-                    <div class="inline-block text-center max-960">
-                        <h4>Master the Basics</h4>
-                        <h4>Get More Done</h4>
-                        <h4>Troubleshooting</h4>
-                    </div>
-                    <div class="inline-block medium-blue-background white half">
-                        <img src="<?php bloginfo('template_directory'); ?>/library/images/white_left_arrow.png" alt="">
-                        <div class="block">
-                            <p class="small">previous</p>
-                            <p>Jamooz Home</p>
+                    <?php if (is_home()) { ?>
+
+                        <div class="inline-block text-center max-960">
+                            <a href="<?php echo get_site_url() ?>/category/setup-basics/">
+                                <h4>Master the Basics</h4>
+                            </a>
+                            <a href="<?php echo get_site_url() ?>/category/do-more/">
+                                <h4>Get More Done</h4>
+                            </a>
+                            <a href="<?php echo get_site_url() ?>/category/troubleshooting/">
+                                <h4>Troubleshooting</h4>
+                            </a>
                         </div>
+                    <?php } ?>
+                    
+                    <div class="medium-blue-background white half">
+                        <a href="<?php echo get_site_url(); ?>/../index.php" 
+                        class="inline-block">
+                            <img src="<?php bloginfo('template_directory'); ?>/library/images/white_left_arrow.png" alt="">
+                            <div class="block">
+                                <p class="small">previous</p>
+                                <p>Jamooz Home</p>
+                            </div>
+                        </a>
                     </div>
+                    
+                    
                     <div class="text-right orange-background white inline-block half">
-                        <div class="block">
-                            <p class="small">next</p>
-                            <p>Getting Started</p>
-                        </div>
-                        
-                        <img src="<?php bloginfo('template_directory'); ?>/library/images/white_right_arrow.png" alt="">
+                        <a href="<?php echo get_site_url(); ?>/../get-started.php"
+                        class="inline-block">
+                            <div class="block">
+                                <p class="small">next</p>
+                                <p>Getting Started</p>
+                            </div>
+                            <img src="<?php bloginfo('template_directory'); ?>/library/images/white_right_arrow.png" alt="">
+                        </a>
                     </div>
 				</div>
 
