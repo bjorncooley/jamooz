860e2a82c9063dfc7dc4b185d40f4254f9d92661
Bjorn Cooley <bjorncooley@gmail.com>
2015-06-25 17:36:34 -0700

    Update Success center header styles

--- success/wp-content/themes/bones/header.php
+++ success/wp-content/themes/bones/header.php
@@ -14,9 +14,9 @@
 		<title><?php wp_title(''); ?></title>
 
 		<?php // mobile meta (hooray!) ?>
-		<!-- <meta name="HandheldFriendly" content="True">
+		<meta name="HandheldFriendly" content="True">
 		<meta name="MobileOptimized" content="320">
-		<meta name="viewport" content="width=device-width, initial-scale=1"/> -->
+		<meta name="viewport" content="width=device-width, initial-scale=1">
 
 		<?php // icons & favicons (for more: http://www.jonathantneal.com/blog/understand-the-favicon/) ?>
 		<link rel="apple-touch-icon" href="<?php echo get_template_directory_uri(); ?>/library/images/apple-touch-icon.png">
@@ -53,40 +53,23 @@
                             <img src="<?php bloginfo('template_directory'); ?>/library/images/success-logo.png" alt="">
                         </a>
                     </div>
+                    <div class="hidden-xs hidden-sm clearfix">
 
-                    <div class="main-header">
-                        <div id="header-search" <?php if (is_home()) { echo "class=hidden"; } ?>>
-                            <?php include (TEMPLATEPATH . '/searchform.php'); ?>
+                        <div class="main-header">
+                            <div id="header-search" <?php if (is_home()) { echo "class=hidden"; } ?>>
+                                <?php include (TEMPLATEPATH . '/searchform.php'); ?>
+                            </div>
+
+                            <?php $url = get_site_url(); ?>
+                            <?php $link = $url . '/../index.php#trial' ?>
+                            <button class="radius large-3 columns medium-dark-gray-border no-background medium-dark-gray small-centered uppercase login-button">Login</button>
+                            <a href="<?php echo $link ?>">
+                                <button class="radius large-3 columns small-centered orange no-background red-orange-border uppercase">Sign Up</button>
+                            </a>
                         </div>
-
-                        <?php $url = get_site_url(); ?>
-                        <?php $link = $url . '/../index.php#trial' ?>
-                        <button class="radius large-3 columns medium-dark-gray-border no-background medium-dark-gray small-centered uppercase login-button">Login</button>
-                        <a href="<?php echo $link ?>">
-                            <button class="radius large-3 columns small-centered orange no-background red-orange-border uppercase">Sign Up</button>
-                        </a>
-
-
-                        <nav role="navigation" itemscope itemtype="http://schema.org/SiteNavigationElement">
-                            <?php wp_nav_menu(array(
-                                     'container' => false,                           // remove nav container
-                                     'container_class' => 'menu cf',                 // class of container (should you choose to use it)
-                                     'menu' => __( 'The Main Menu', 'bonestheme' ),  // nav name
-                                     'menu_class' => 'nav top-nav cf',               // adding custom nav class
-                                     'theme_location' => 'main-nav',                 // where it's located in the theme
-                                     'before' => '',                                 // before the menu
-                                       'after' => '',                                  // after the menu
-                                       'link_before' => '',                            // before each link
-                                       'link_after' => '',                             // after each link
-                                       'depth' => 0,                                   // limit the depth of the nav
-                                     'fallback_cb' => ''                             // fallback function (if there is one)
-                            )); ?>
-
-                        </nav>
                     </div>
 
                     
-
 				</div>
 
 			</header>
