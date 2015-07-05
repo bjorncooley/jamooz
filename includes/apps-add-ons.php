2ea5dec878ce658787d429e59fd1defd09090ca7
Bjorn Cooley <bjorncooley@gmail.com>
2015-07-04 18:20:24 -0700

    Update remaining add-on products with data attributes for plan profile functions

--- includes/apps-add-ons.php
+++ includes/apps-add-ons.php
@@ -26,7 +26,7 @@
                 <img src="img/pricing/efax_icon.png" alt="">
             </div>
             <div class="small-12 large-6 columns">
-                <h1 class="no-text-transform light">Call Center Queue &amp; Reporting</h1>
+                <h1 class="no-text-transform light product-title" data-price="200">Call Center Queue &amp; Reporting</h1>
                 <p class="light">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Excepturi libero sed, facere a ipsam. Eveniet iure, recusandae eligendi, ex magni aperiam ipsa provident quam dicta quae temporibus voluptate porro eos?</p>
             </div>
             <div class="small-12 columns text-center add-container">
@@ -41,7 +41,7 @@
         <img src="img/shared/slider_left_arrow_white.png" alt="" class="left-arrow slide-left-arrow">
         <div class="row">
             <div class="small-12 large-6 columns">
-                <h1 class="no-text-transform light">Call Recording.</h1>
+                <h1 class="no-text-transform light product-title" data-price="200">Call Recording.</h1>
                 <p class="light">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Excepturi libero sed, facere a ipsam. Eveniet iure, recusandae eligendi, ex magni aperiam ipsa provident quam dicta quae temporibus voluptate porro eos?</p>
             </div>
             <div class="small-12 large-6 columns show-for-large-up">
@@ -62,7 +62,7 @@
                 <img src="img/pricing/efax_icon.png" alt="">
             </div>
             <div class="small-12 large-6 columns">
-                <h1 class="no-text-transform light">Administrative App</h1>
+                <h1 class="no-text-transform light product-title" data-price="200">Administrative App</h1>
                 <p class="light">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Excepturi libero sed, facere a ipsam. Eveniet iure, recusandae eligendi, ex magni aperiam ipsa provident quam dicta quae temporibus voluptate porro eos?</p>
             </div>
             <div class="small-12 columns text-center add-container">
@@ -77,7 +77,7 @@
         <img src="img/shared/slider_left_arrow_white.png" alt="" class="left-arrow slide-left-arrow">
         <div class="row">
             <div class="small-12 large-6 columns">
-                <h1 class="no-text-transform light">Auto Attendant.</h1>
+                <h1 class="no-text-transform light product-title" data-price="200">Auto Attendant.</h1>
                 <p class="light">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Excepturi libero sed, facere a ipsam. Eveniet iure, recusandae eligendi, ex magni aperiam ipsa provident quam dicta quae temporibus voluptate porro eos?</p>
             </div>
             <div class="small-12 large-6 columns show-for-large-up">
